e76b829562715de2f71aca85849c59148352b781
MierenManz <christiaanmens@gmail.com>
2022-05-15 18:16:37 +0200

    add import section

--- wasm_encoder/src/sections/import_section.rs
+++ wasm_encoder/src/sections/import_section.rs
@@ -1,15 +1,16 @@
 use super::Section;
 use crate::EncodingError;
+use leb128::write;
 
 pub enum ImportKind {
     Function = 0x00,
     Table = 0x1,
     Memory = 0x02,
-    Global = 0x03
+    Global = 0x03,
 }
 
 pub struct ImportSection {
-    imports: Vec<(String, String, ImportKind)>
+    imports: Vec<(String, String, ImportKind)>,
 }
 
 impl ImportSection {
@@ -17,8 +18,14 @@ impl ImportSection {
         Self::default()
     }
 
-    pub fn add_import<T: Into<String>>(&mut self, namespace: T, fn_name: T, kind: ImportKind) -> usize {
-        self.imports.push((namespace.into(), fn_name.into(), kind));
+    pub fn add_import<T: Into<String>>(
+        &mut self,
+        module_name: T,
+        export_name: T,
+        kind: ImportKind,
+    ) -> usize {
+        self.imports
+            .push((module_name.into(), export_name.into(), kind));
 
         self.imports.len() - 1
     }
@@ -43,10 +50,23 @@ impl Default for ImportSection {
 
 impl Section for ImportSection {
     fn compile(self) -> Result<Vec<u8>, EncodingError> {
-        Ok(Vec::new())
+        let mut byte_buff = Vec::new();
+        write::unsigned(&mut byte_buff, self.imports.len() as u64)?;
+
+        for (module_name, export_name, kind) in self.imports {
+            write::unsigned(&mut byte_buff, module_name.len() as u64)?;
+            byte_buff.extend_from_slice(module_name.as_bytes());
+
+            write::unsigned(&mut byte_buff, export_name.len() as u64)?;
+            byte_buff.extend_from_slice(export_name.as_bytes());
+
+            byte_buff.push(kind as u8);
+        }
+
+        Ok(byte_buff)
     }
 
-    fn id() -> u8 {
+    fn id(&self) -> u8 {
         0x02
     }
 }
